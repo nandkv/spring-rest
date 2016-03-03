ea5ae1bad6bb0b685800999fed0fde448eef3ba0
nandkv <nandkv.online@gmail.com>
2016-03-03 08:45:05 -0600

    Update PersistenceConfig.java

--- src/main/java/com/spring/rest/config/PersistenceConfig.java
+++ src/main/java/com/spring/rest/config/PersistenceConfig.java
@@ -55,7 +55,7 @@ public class PersistenceConfig {
 		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
 		dataSource.setUrl("jdbc:mysql://localhost:3306/spring_rest");
 		dataSource.setUsername("root");
-		dataSource.setPassword("sairam76");
+		dataSource.setPassword("******");
 		return dataSource;
 	}
 
